cf1431012e8beccd3568cc6c7a503a9f6594523f
anphong1808 <anphong.viettech@gmail.com>
2026-05-10 13:57:01 +0700

    fix(portal): export deletePortalHandler from portal handlers index

--- src/handlers/portal/index.ts
+++ src/handlers/portal/index.ts
@@ -1,2 +1,3 @@
 export { handler as listPortalsHandler } from './listPortalsHandler';
 export { handler as transferPortalHandler } from './transferPortalHandler';
+export { handler as deletePortalHandler } from './deletePortalHandler';
